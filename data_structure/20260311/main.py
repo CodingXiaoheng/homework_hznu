5cc0cb5c2e8a7511bf2c7bff7684c1a0293b8dbd
CodingXiaoheng <github@codingxiaoheng.me>
2026-03-16 20:31:32 +0800

    报告成品

--- data_structure/20260311/main.py
+++ data_structure/20260311/main.py
@@ -1,30 +1,33 @@
 from student import Student,StudentFeatureTest
-from sqrt import sqrt as sqrt
+from sqrt import sqrt
 
 test = StudentFeatureTest()
 test.test_empty_grades()
 test.test_invalid_input()
 test.test_instance_independences()
 
-
+# 第一个学生
 stu = Student("Tom", 19)
-
-# stu.add_grade(80)
-# stu.add_grade(90)
+# 空分数测试
+print(stu.get_average_grade())
+# 输入转换测试
 stu.add_grade("63.281")
 
+# 第二个学生
 stu2 = Student("Jerry", 20)
-
-# stu.add_grade(80)
-# stu.add_grade(90)
+stu2.add_grade(80)
+stu2.add_grade(90)
 stu2.add_grade(72)
 
-print(stu.get_details())
-
+#封装测试
+stu.grades.clear()
 print(stu.grades)
-
-print(stu2.get_details())
-
 print(stu2.grades)
 
+
+# 独立性测试
+print(stu.get_details())
+print(stu2.get_details())
+
+# 扩展题
 print(sqrt(4))
